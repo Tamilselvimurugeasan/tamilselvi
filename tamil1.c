0c54cfb5090cac0b9d9920934c934d3b4a91498d
Tamilselvimurugeasan <tamilselvi.mece@ksrce.ac.in>
2017-04-19 14:46:41 +0530

    Add files via upload

---
+++ tamil1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the  number");
+scanf("%d",&n);
+if(n>0)
+printf("\n the given number is positive");
+if(n<0)
+printf("\n the given number is negative");
+else
+printf("\n zero");
+return 0;
+}
