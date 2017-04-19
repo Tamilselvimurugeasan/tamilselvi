462cea16bd7704e0a2b51afbef20185362308de4
Tamilselvimurugeasan <tamilselvi.mece@ksrce.ac.in>
2017-04-19 14:34:01 +0530

    Add files via upload

---
+++ tamil.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d",&num);
+if(num%2==0)
+printf("enter a number is even");
+else
+printf("enter the number is odd");
+return 0;
+}
