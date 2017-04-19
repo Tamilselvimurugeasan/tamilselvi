259ec838056e6508bdfe8014a08a7cf5e27eb4d6
Tamilselvimurugeasan <tamilselvi.mece@ksrce.ac.in>
2017-04-19 15:24:23 +0530

    Add files via upload

---
+++ tamil4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+int n1,n2;
+printf("enter the alphabet and two numbers:");
+scanf("%c %d %d",&c,&n1,&n2);
+printf("\n the alphabet is:%c number is:%d %d",c,n1,n2);
+return 0;}
